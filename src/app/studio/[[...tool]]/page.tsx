694d69ee9b6462f579cf470fc49569c64f6aec7c
Daniella Oji <Info@daniellauche-oji.com>
2026-03-31 02:47:45 -0500

    Sanity studio fix

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,19 +1,7 @@
 'use client';
 
 import { NextStudio } from 'next-sanity/studio';
-import { defineConfig } from 'sanity';
-import { structureTool } from 'sanity/structure';
-import { visionTool } from '@sanity/vision';
-import { schemaTypes } from '../../../../schemas/siteConfig';
-
-
-const config = defineConfig({
-  basePath: '/studio',
-  projectId: 'pm3o6fcp',
-  dataset: 'production',
-  schema: { types: schemaTypes },
-  plugins: [structureTool(), visionTool()],
-});
+import config from '../../../../sanity.config';
 
 export default function StudioPage() {
   return <NextStudio config={config} />;
